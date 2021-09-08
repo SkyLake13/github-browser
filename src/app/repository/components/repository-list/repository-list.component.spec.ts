07b58cc050d9869d81e59a5b11bc0907925f7ffa
Abhishek Kumar <skylake1302@gmail.com>
2021-09-08 21:01:05 +0200

    Added more unit tests

--- src/app/repository/components/repository-list/repository-list.component.spec.ts
+++ src/app/repository/components/repository-list/repository-list.component.spec.ts
@@ -1,21 +1,66 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterModule } from '@angular/router';
+import { PageEvent } from '@angular/material/paginator';
+import { Router, RouterModule } from '@angular/router';
+import { of } from 'rxjs';
 import { RepositoryService } from '../../services/repository.service';
 import { RepositoryListComponent } from './repository-list.component';
 
+const searchReposResponsePage_1 = {
+  count: 1000,
+  items: [
+    {
+      name: 'facebook/jest',
+      language: 'javascript',
+      stars: 83729,
+      createdAt: new Date(Date.now()),
+      avatarUrl: 'https://avatars.githubusercontent.com/u/16378997?v=4'
+    },
+    {
+      name: 'facebook/react',
+      language: 'typescript',
+      stars: 282011,
+      createdAt: new Date(Date.now()),
+      avatarUrl: 'https://avatars.githubusercontent.com/u/16378997?v=4'
+    }
+  ]
+}
+
+const searchReposResponsePage_2 = {
+  count: 1000,
+  items: [
+    {
+      name: 'facebook/jest',
+      language: 'javascript',
+      stars: 83729,
+      createdAt: new Date(Date.now()),
+      avatarUrl: 'https://avatars.githubusercontent.com/u/16378997?v=4'
+    },
+    {
+      name: 'facebook/react',
+      language: 'typescript',
+      stars: 282011,
+      createdAt: new Date(Date.now()),
+      avatarUrl: 'https://avatars.githubusercontent.com/u/16378997?v=4'
+    },
+    {
+      name: 'facebook/react',
+      language: 'typescript',
+      stars: 282011,
+      createdAt: new Date(Date.now()),
+      avatarUrl: 'https://avatars.githubusercontent.com/u/16378997?v=4'
+    }
+  ]
+}
 
 const fakeRepoService = {
-  getCommits: () => {
-
-  },
-  searchCommits: () => {
-
-  }
+  search: () => of()
 }
 
 describe('RepositoryListComponent', () => {
   let component: RepositoryListComponent;
   let fixture: ComponentFixture<RepositoryListComponent>;
+  let service: RepositoryService;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -36,9 +81,92 @@ describe('RepositoryListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(RepositoryListComponent);
     component = fixture.componentInstance;
+    service = TestBed.inject(RepositoryService);
+    router = TestBed.inject(Router);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should search reposiotries', () => {
+    const spy = spyOn<any, any>(service, 'search')
+                  .and.returnValue(of(searchReposResponsePage_1));
+
+    component.searchTextChange('facebook');
+    fixture.detectChanges();
+
+    expect(component.repos.count).toBe(1000);
+    expect(component.dataSource).toBeTruthy();
+    expect(component.dataSource.length).toBe(2);
+  });
+
+  it('should navigate to commits route', () => {
+    spyOn<any, any>(service, 'search')
+                  .and.returnValue(of(searchReposResponsePage_1));
+
+    const routerSpy = spyOn(router, 'navigate');
+
+    component.searchTextChange('facebook');
+    fixture.detectChanges();
+    component.rowClick(searchReposResponsePage_1.items[0]);
+    fixture.detectChanges();
+
+    expect(routerSpy).toHaveBeenCalledWith(['commits', 'facebook/jest']);
+  });
+
+  it('should filter by language', () => {
+    const spy = spyOn<any, any>(service, 'search')
+                  .and.returnValue(of(searchReposResponsePage_1));
+
+    component.searchTextChange('facebook');
+    fixture.detectChanges();
+    component.languageSelectionChange(['javascript']);
+    fixture.detectChanges();
+
+    expect(component.dataSource).toBeTruthy();
+    expect(component.dataSource.length).toBe(1);
+    expect(component.dataSource[0].language).toBe('javascript');
+  });
+
+  it('should filter by stars', () => {
+    const spy = spyOn<any, any>(service, 'search')
+                  .and.returnValue(of(searchReposResponsePage_1));
+
+    component.searchTextChange('facebook');
+    fixture.detectChanges();
+
+    expect(component.dataSource).toBeTruthy();
+    expect(component.dataSource.length).toBe(2);
+
+    component.minimumStarsValueChange(200000);
+    fixture.detectChanges();
+
+    expect(component.dataSource).toBeTruthy();
+    expect(component.dataSource.length).toBe(1);
+    expect(component.dataSource[0].language).toBe('typescript');
+  });
+
+  it('should fetch next page of searched reposiotries', () => {
+    const spy = spyOn<any, any>(service, 'search')
+                  .and.returnValue(of(searchReposResponsePage_1));
+
+    component.searchTextChange('facebook');
+    fixture.detectChanges();
+
+    expect(component.repos.count).toBe(1000);
+    expect(component.dataSource).toBeTruthy();
+    expect(component.dataSource.length).toBe(2);
+
+    spy.and.returnValue(of(searchReposResponsePage_2));
+
+    const pageEvent = new PageEvent();
+    pageEvent.pageIndex = 1;
+    component.pageChange(pageEvent);
+    fixture.detectChanges();
+
+    expect(component.repos.count).toBe(1000);
+    expect(component.dataSource).toBeTruthy();
+    expect(component.dataSource.length).toBe(3);
   });
 });
--- src/app/shared/components/search/search.component.spec.ts
+++ src/app/shared/components/search/search.component.spec.ts
@@ -2,16 +2,13 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { SearchComponent } from './search.component';
-import { Subscription } from 'rxjs';
-import { By } from '@angular/platform-browser';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
-  let subscription = new Subscription();
   
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -32,27 +29,7 @@ describe('SearchComponent', () => {
     fixture.detectChanges();
   });
 
-  afterEach(() => {
-    subscription.unsubscribe();
-  });
-
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  xit('should emit search text', (done: DoneFn) => {
-    const input = 'facebook';
-
-    const subs =  component.search.subscribe((searchText) => {
-      expect(searchText).toBe(input);
-
-      done();
-    });
-    subscription.add(subs);
-
-    const textBox = fixture.debugElement.query(By.css('input'));
-    textBox.nativeElement.value = input;
-    textBox.triggerEventHandler('input', { target: textBox.nativeElement });
-    fixture.detectChanges();
   });
 });
